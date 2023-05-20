a53cde42ce829e892edd4480a5ce4b6a952021a0
Maciej Garbacz <61703252+garbaczmaciej@users.noreply.github.com>
2023-05-20 15:21:54 +0200

    2015-16

--- 2015/16/solve.py
+++ 2015/16/solve.py
@@ -3,21 +3,72 @@ from sys import argv
 from datetime import datetime
 
 
+GREATER_THAN_READINGS = ["cats", "trees"]
+FEWER_THAN_READINGS = ["pomeranians", "goldfish"]
+
+
+def is_possible_aunt(aunt: dict, requirements: dict) -> bool:
+    return all(value == requirements[attr] for attr, value in aunt.items())
+
+
+def is_possible_aunt_p2(aunt: dict, requirements: dict) -> bool:
+    for attr, value in aunt.items():
+        if attr in GREATER_THAN_READINGS:
+            if value <= requirements[attr]:
+                return False
+        elif attr in FEWER_THAN_READINGS:
+            if value >= requirements[attr]:
+                return False
+        else:
+            if value != requirements[attr]:
+                return False
+    return True
+
+
+def get_requirements(filename: str):
+    with open(filename, "r") as f:
+        requirements = {
+            line.split(": ")[0] : int(line.split(": ")[1])
+            for line in f.read().split("\n") 
+            if line.split()
+        }
+    return requirements
+
+
 def get_input(filename: str):
     with open(filename, "r") as f:
-        _in = [line for line in f.read().split("\n") if line.split()]
-        # For 1 line inputs
-        # _in = f.read().replace("\n", "").strip()
+        _in = {
+            int(line.split()[1][:-1]) : {
+                component.split(": ")[0]: int(component.split(": ")[1])
+                for component in " ".join(
+                    line.split()[2:]
+                ).split(", ")
+            }
+            for line in f.read().split("\n") 
+            if line.split()
+        }
 
     return _in
 
 
 def part1(_in):
-    pass
+    requirements = get_requirements("req.txt")
+    possible_aunts = [
+        (n, aunt) 
+        for n, aunt in _in.items() 
+        if is_possible_aunt(aunt, requirements)
+    ]
+    return possible_aunts[0]
 
 
 def part2(_in):
-    pass
+    requirements = get_requirements("req.txt")
+    possible_aunts = [
+        (n, aunt) 
+        for n, aunt in _in.items() 
+        if is_possible_aunt_p2(aunt, requirements)
+    ]
+    return possible_aunts[0]
 
 
 def benchmark(name: str, func, *_in) -> None:
