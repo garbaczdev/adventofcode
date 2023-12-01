16d3c7bd64ea7b3dcbaa4ad0e0600beec894564c
garbaczmaciej <contact@garbacz.dev>
2023-12-01 12:37:25 +0100

    Automated fetching of aoc inputs

--- 2023/create_template.sh
+++ 2023/create_template.sh
@@ -4,3 +4,6 @@ cat ./template.py > $1/solve.py
 touch $1/input.txt
 touch $1/sample_input.txt
 
+# Arch Specific - Automated input downloading
+source /disk/linux/python3-venvs/aoc-env/bin/activate
+aocd $1 2023 > $1/input.txt
