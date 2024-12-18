9da7c9bf1d448654e9d15f9a573486cae9d00b3b
garbaczdev <maciej@garbacz.dev>
2024-12-19 00:34:44 +0100

    2024-16 part 2

--- 2024/16/solve.go
+++ 2024/16/solve.go
@@ -9,6 +9,15 @@ import (
 	"time"
 )
 
+
+var ALL_DIRECTIONS []Vector2D = []Vector2D{
+    Vector2D{X: 0, Y: -1},
+    Vector2D{X: 1, Y: 0},
+    Vector2D{X: 0, Y: 1},
+    Vector2D{X: -1, Y: 0},
+}
+
+
 // Extending the vector struct
 func (v Vector2D) ToMazeCostTableIndex() int {
     if v.Equals(Vector2D{X: 0, Y: -1}) {
@@ -52,24 +61,42 @@ func (r Reindeer) RotateCounterClockwise() Reindeer {
 }
 
 
-type MazeCostTable [][][4]int
-
-func (m MazeCostTable) GetCost(r Reindeer) int {
-    return m[r.Position.Y][r.Position.X][r.Direction.ToMazeCostTableIndex()]
+type mazeCostTableEntry struct {
+    cost int
+    from []Reindeer
 }
 
-func (m MazeCostTable) UpdateCost(r Reindeer, newCost int) {
-    m[r.Position.Y][r.Position.X][r.Direction.ToMazeCostTableIndex()] = newCost
+// This is a structure used to keep the shortest path to (position, direction) for dijkstra's algorithm
+type MazeCostTable [][][4]mazeCostTableEntry
+
+func (m MazeCostTable) GetCost(r Reindeer) int {
+    return m[r.Position.Y][r.Position.X][r.Direction.ToMazeCostTableIndex()].cost
+}
+
+func (m MazeCostTable) GetEntry(r Reindeer) (int, []Reindeer) {
+    cell := &m[r.Position.Y][r.Position.X][r.Direction.ToMazeCostTableIndex()]
+    return cell.cost, cell.from
+}
+
+func (m MazeCostTable) UpdateCost(r Reindeer, previousR Reindeer, newCost int) {
+    cell := &m[r.Position.Y][r.Position.X][r.Direction.ToMazeCostTableIndex()]
+
+    if newCost != cell.cost {
+        cell.from = make([]Reindeer, 0, 1)
+    }
+
+    cell.from = append(cell.from, previousR) 
+    cell.cost = newCost
 }
 
 func NewMazeCostTable(maze [][]byte) MazeCostTable {
-    mazeCostTable := make([][][4]int, 0, len(maze))
+    mazeCostTable := make([][][4]mazeCostTableEntry, 0, len(maze))
     for _, mazeRow := range maze {
-        mazeRowCostTable := make([][4]int, 0, len(mazeCostTable))
+        mazeRowCostTable := make([][4]mazeCostTableEntry, 0, len(mazeCostTable))
         for range mazeRow {
-            var mazeTileCostTable [4]int
+            var mazeTileCostTable [4]mazeCostTableEntry
             for i := range 4 {
-                mazeTileCostTable[i] = math.MaxInt
+                mazeTileCostTable[i].cost = math.MaxInt
             }
             mazeRowCostTable = append(mazeRowCostTable, mazeTileCostTable)
         }
@@ -79,7 +106,14 @@ func NewMazeCostTable(maze [][]byte) MazeCostTable {
 }
 
 
-func findShortestPathLength(maze [][]byte) int {
+// Used as a structure kept in the priority queue
+type priorityQueueReindeerEntry struct {
+    PreviousReindeer Reindeer
+    Reindeer Reindeer
+}
+
+
+func findShortestPathLength(maze [][]byte) (int, MazeCostTable) {
     var reindeerPosition Vector2D
     for y, mazeRow := range maze {
         for x, tile := range mazeRow {
@@ -95,22 +129,31 @@ func findShortestPathLength(maze [][]byte) int {
 
     mazeCostTable := NewMazeCostTable(maze)
 
-    pq := NewReversePriorityQueue[Reindeer]()
-    pq.Push(startReindeer, 0)
+    pq := NewReversePriorityQueue[priorityQueueReindeerEntry]()
+    pq.Push(priorityQueueReindeerEntry{Reindeer: startReindeer, PreviousReindeer: startReindeer}, 0)
+
+    pathFound := false
+    minPathCost := math.MaxInt
 
     for !pq.Empty() {
-        reindeer, cost := pq.Pop()
-        // fmt.Println(reindeer, cost)
+        pqEntry, cost := pq.Pop()
+        reindeer := pqEntry.Reindeer
 
         // A reindeer with a lower cost has already been here
-        if cost >= mazeCostTable.GetCost(reindeer) {
+        if cost > mazeCostTable.GetCost(reindeer) {
+            // The cost has been exceeded and the minpath has been found, so stop the algorithm
+            if pathFound {
+                break
+            }
             continue
         }
-        mazeCostTable.UpdateCost(reindeer, cost)
+        mazeCostTable.UpdateCost(reindeer, pqEntry.PreviousReindeer, cost)
 
         // Arrived at the finish point!
         if reindeer.GetTile(maze) == 'E' {
-            return cost
+            pathFound = true
+            minPathCost = cost
+            continue
         }
         
         movedReindeer := reindeer.Move()
@@ -118,17 +161,17 @@ func findShortestPathLength(maze [][]byte) int {
         counterClockwiseRotatedReindeer := reindeer.RotateCounterClockwise()
         
         if movedReindeer.GetTile(maze) != '#' && cost + 1 < mazeCostTable.GetCost(movedReindeer) {
-            pq.Push(movedReindeer, cost + 1)
+            pq.Push(priorityQueueReindeerEntry{Reindeer: movedReindeer, PreviousReindeer: reindeer}, cost + 1)
         }
         if clockwiseRotatedReindeer.GetTile(maze) != '#' && cost + 1000 < mazeCostTable.GetCost(clockwiseRotatedReindeer) {
-            pq.Push(clockwiseRotatedReindeer, cost + 1000)
+            pq.Push(priorityQueueReindeerEntry{Reindeer: clockwiseRotatedReindeer, PreviousReindeer: reindeer}, cost + 1000)
         }
         if counterClockwiseRotatedReindeer.GetTile(maze) != '#' && cost + 1000 < mazeCostTable.GetCost(counterClockwiseRotatedReindeer) {
-            pq.Push(counterClockwiseRotatedReindeer, cost + 1000)
+            pq.Push(priorityQueueReindeerEntry{Reindeer: counterClockwiseRotatedReindeer, PreviousReindeer: reindeer}, cost + 1000)
         }
     }
     
-    panic("No path to finish")
+    return minPathCost, mazeCostTable
 }
 
 
@@ -158,12 +201,86 @@ func printMaze(maze [][]byte) {
 
 func part1(input string) int {
     maze := parseInput(input)
-    return findShortestPathLength(maze)
+    minCost, _ := findShortestPathLength(maze)
+    return minCost
+}
+
+
+func backtrackMaze(maze [][]byte, mazeCostTable MazeCostTable, mazeBitmap MazeCostTable, reindeer Reindeer) {
+    // Mark the position as visited
+    if mazeBitmap.GetCost(reindeer) == 0 || reindeer.GetTile(maze) == 'S' {
+        return
+    }
+    mazeBitmap.UpdateCost(reindeer, reindeer , 0)
+
+    _, previousReindeers := mazeCostTable.GetEntry(reindeer)
+    for _, previousReindeer := range previousReindeers {
+        backtrackMaze(maze, mazeCostTable, mazeBitmap, previousReindeer)
+    }
+}
+
+
+func getNumberOfTilesOnMinPaths(maze [][]byte, mazeCostTable MazeCostTable) int {
+    // Find end position
+    var endPosition Vector2D
+    for y, mazeRow := range maze {
+        for x, tile := range mazeRow {
+            if tile == 'E' {
+                endPosition = Vector2D{X: x, Y: y}
+            }
+        }
+    }
+    // Create bitmap for visited tiles
+    mazeBitmap := NewMazeCostTable(maze)
+
+    minCost := math.MaxInt
+    for _, direction := range ALL_DIRECTIONS {
+        cost := mazeCostTable.GetCost(Reindeer{Position: endPosition, Direction: direction})
+        if cost < minCost {
+            minCost = cost
+        }
+    }
+
+    for _, direction := range ALL_DIRECTIONS {
+        cost, previousReindeers := mazeCostTable.GetEntry(Reindeer{Position: endPosition, Direction: direction})
+        if cost != minCost {
+            continue
+        }
+        for _, previousReindeer := range previousReindeers {
+            backtrackMaze(maze, mazeCostTable, mazeBitmap, previousReindeer)
+        }
+    }
+
+    // Includes start and end
+    visitedTilesCount := 2
+    for y, mazeRow := range maze {
+        for x, tile := range mazeRow {
+            isTileVisited := false
+            for _, direction := range ALL_DIRECTIONS {
+                if mazeBitmap.GetCost(Reindeer{Position: Vector2D{X: x, Y: y}, Direction: direction}) == 0 {
+                    isTileVisited = true
+                    break
+                }
+            }
+            
+            if isTileVisited {
+                visitedTilesCount++
+                fmt.Print("O")
+            } else {
+                fmt.Print(string(tile))
+            }
+        }
+        fmt.Print("\n")
+    }
+
+    return visitedTilesCount
 }
 
 
 func part2(input string) int {
-    return 0
+    maze := parseInput(input)
+    _, mazeCostTable := findShortestPathLength(maze)
+    return getNumberOfTilesOnMinPaths(maze, mazeCostTable)
 }
 
 
